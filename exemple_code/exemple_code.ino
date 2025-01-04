ffb6670a048655cca8ab2f3a3fc97b8d88d7a97a
idir <idir.ait@gmail.com>
2025-01-04 21:03:58 +0100

    save

--- exemple_code/exemple_code.ino
+++ exemple_code/exemple_code.ino
@@ -1,20 +1,72 @@
+//Lib wifi server
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include "AsyncJson.h"
 #include "ArduinoJson.h"
+//--------------------------------------------------------------------------------------------------
+//Lib stepper motor
+#include <Stepper.h> //AccelStepper
+#include <ESP32Servo.h>
+#include "soc/rtc_wdt.h"
+//--------------------------------------------------------------------------------------------------
+
+// Variables Wifi
 AsyncWebServer server(80);
-const char *ssid = "Freebox-44E6E2";
-const char *password = "wifi@temporaire@71";
+const char *SSID = "Freebox-44E6E2";
+const char *PASSWORD = "wifi@temporaire@71";
+
+// Variables stepper motor 
+//--------------------------------------------------------------------------------------------------
+const int STEPS_PER_REVOLUTION = 2048;  // change this to fit the number of steps per revolution
+
+// ULN2003 Motor Driver Pins
+const int IN1 = 19;
+const int IN2 = 18;
+const int IN3 = 5;
+const int IN4 = 17;
+
+static const int SERVO_PIN = 16;
+Stepper STEEPER(STEPS_PER_REVOLUTION, IN1, IN3, IN2, IN4);
+
+int askedSteps = 0;
+
+//--------------------------------------------------------------------------------------------------
+
+
+// initialize the stepper library
+void initStepper() 
+{
+  // set the speed at 5 rpm
+  STEEPER.setSpeed(5);
+  // initialize the serial port
+  Serial.begin(9600);
+}
+
+void moveStepperTo(int steps)
+{
+  STEEPER.step(steps);
+  Serial.print("Nombre de pas: ");
+  Serial.println(steps);
+}
+
+//--------------------------------------------------------------------------------------------------
+
 void notFound(AsyncWebServerRequest *request)
 {
   request->send(404, "application/json", "{\"message\":\"Not found\"}");
 }
+
+//--------------------------------------------------------------------------------------------------
 void setup()
 {
+  // Stepper init 
+  initStepper();
+
+  // server Wifi init 
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
     Serial.printf("WiFi Failed!\n");
@@ -28,6 +80,8 @@ void setup()
     if (request->hasParam("message"))
     {
       data["message"] = request->getParam("message")->value();
+      Serial.println(request->getParam("message")->value().toInt());
+      askedSteps = request->getParam("message")->value().toInt();
     }
     else
     {
@@ -54,8 +108,14 @@ void setup()
   });
   server.addHandler(handler);server.onNotFound(notFound);
   server.begin();
-}void loop()
+}
+
+void loop()
 {
+  if(askedSteps != 0){
+    moveStepperTo(askedSteps);
+    askedSteps = 0;
+  }
 }
 
 
